189a68c5395099750baef532a6c42c5e7ba77698
jonjitsu13 <72325168+jonjitsu13@users.noreply.github.com>
2020-12-24 14:49:40 -0500

    commit

---
+++ faceitcalls.js
@@ -0,0 +1,10 @@
+let url = 'https://open.faceit.com/data/v4/players?nickname=Static_X';
+let bearer = 'Bearer 8c0f3148-1ea1-43d5-be95-fec82dd42948';
+fetch(url, {
+        headers: {
+            'Authorization': bearer,
+            'Content-Type': 'application/json'
+        }
+        })
+    .then(response => response.json())
+    .then(playerDetails => alert(playerDetails.nickname));
